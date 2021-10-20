c586e3f57196b6918b9438ea3560ed0c2ebb0c98
igonzales21 <rmg41996@yahoo.com>
2021-10-20 00:29:42 -0400

    worked on app and fixed files

--- app/app.py
+++ app/app.py
@@ -1,5 +1,4 @@
 """
-Modified from https://github.com/tatiblockchain/python-deep-learning-chatbot
 Modified from Lesson 5.4
 """
 
@@ -19,11 +18,40 @@ def player_submit():
     
     user_input = request.args
     
-    X_test = user_input['player_name']
+    name = user_input['PlayerName']
     
-    salary = pickle.load(open('../model/salary_model.pkl', 'rb'))
+    #Get names of recommended players
+    players = pickle.load(open('../pickles/recommendation.pkl', 'rb'))
+    players = players[name].sort_values()[1:11].index
     
-
+    #Get file with player stats
+    stats = pickle.load(open('../pickles/forecast.pkl', 'rb'))
+    
+    #Get salary based on forecasted stats
+    salary = pickle.load(open('../pickles/pred_salary.pkl', 'rb'))
+    
+    player_salary = salary[salary.index == name]
+    
+    player_salary = round(player_salary['salary'][0], 2)
+    
+    related_players = []
+    
+    headings = stats.columns
+    
+    #Get name and salary of recommended players
+    for player in players: 
+        
+        rec_salary = salary[salary.index == player]
+        
+        rec_salary = round(rec_salary['salary'][0], 2)
+        
+        info_player = stats[stats['name'] == player]
+        
+        info = info_player.values
+        
+        related_players.append({'name':player, 'salary': rec_salary, 'info':info})
+    
+    return render_template('results.html', PlayerName = name, playerSalary = player_salary, related_players = related_players, headings = headings)
 
 
 if __name__ == '__main__':
